e69b08e4d4d5f6e3035b7544c134aef2319f5fbe
Raghul S <rahulkrupa007@gmail.com>
2023-01-16 08:15:04 +0530

    Add files via upload

---
+++ client/server_stream.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"context"
+	"io"
+	"log"
+
+	pb "github.com/Raghulds/go-with-grpc/proto"
+)
+
+func callSayHelloServerStream(client pb.GreetServiceClient, names *pb.NamesList) {
+	log.Printf("Steaming started")
+	stream, err := client.SayHelloServerStreaming(context.Background(), names)
+	if err != nil {
+		log.Fatalf("could not send names: %v", err)
+	}
+	for {
+		message, err := stream.Recv()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			log.Fatalf("error while streaming %v", err)
+		}
+		log.Println(message)
+	}
+	log.Printf("streaming finished")
+}
